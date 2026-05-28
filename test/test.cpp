672f8878df768fc2798234107131b72c6a338c44
Martina Delger <martidelger@gmail.com>
2026-05-28 13:40:05 +0000

    test cases built and passed successfully

--- test/test.cpp
+++ test/test.cpp
@@ -1,121 +1,81 @@
 #include <catch2/catch_test_macros.hpp>
-#include <iostream>
+
+#include <vector>
 
 #include "interquartile_range.h"
-
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be unique, but you can group multiple tests with [tags]. A test can have [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[flag]"){
-	// instantiate any class members that you need to test here
-	int one = 1;
+TEST_CASE("Test 1: Small Even List", "[iqr]") {
+	vector<int> v = {1, 4, 7, 10};
+	Node* head = nullptr;
+	for (int i : v)
+		head = insertEnd(head, i);
 
-	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 1); // fix me!
+	REQUIRE(interQuartile(head) == 6.0f);
 
-	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(true); // also fix me!
+	while (head != nullptr) {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
-TEST_CASE("Test 2", "[flag]"){
-	// you can also use "sections" to share setup code between tests, for example:
-	int one = 1;
+TEST_CASE("Test 2: Odd Length List", "[iqr]") {
+	vector<int> v = {1, 2, 10, 20, 40};
+	Node* head = nullptr;
+	for (int i : v)
+		head = insertEnd(head, i);
 
-	SECTION("num is 2") {
-		int num = one + 1;
-		REQUIRE(num == 2);
-	};
+	REQUIRE(interQuartile(head) == 28.5f);
 
-	SECTION("num is 3") {
-		int num = one + 2;
-		REQUIRE(num == 3);
-	};
-
-	// each section runs the setup code independently to ensure that they don't affect each other
+	while (head != nullptr) {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
-// you must write 5 unique, meaningful tests for credit on the testing portion of this quiz!
-
-// the provided tests from edugator are below. Note that you must determine the correct output for the [output_hidden] tests yourself
-
-TEST_CASE("Function: IQR 1", "[given]") {
-	std::vector<int> v = {2, 4, 4, 5, 6, 7, 8};
+TEST_CASE("Test 3: Equal Values", "[iqr]") {
+	vector<int> v = {5, 5, 5, 5, 5, 5};
 	Node* head = nullptr;
-	for(int i: v)
+	for (int i : v)
 		head = insertEnd(head, i);
 
-	REQUIRE(interQuartile(head) == 3.00);
-	
-    while (head != nullptr) 
-    {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
+	REQUIRE(interQuartile(head) == 0.0f);
+
+	while (head != nullptr) {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
-TEST_CASE("Function: IQR 2", "[given]") {
-	std::vector<int> v = {1, 2, 3, 4};
+TEST_CASE("Test 4: Negative Values", "[iqr]") {
+	vector<int> v = {-10, -5, 0, 5, 10, 15, 20, 25};
 	Node* head = nullptr;
-	for(int i: v)
+	for (int i : v)
 		head = insertEnd(head, i);
 
-	REQUIRE(interQuartile(head) == 2.00);
-	
-    while (head != nullptr) 
-    {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
+	REQUIRE(interQuartile(head) == 20.0f);
+
+	while (head != nullptr) {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
 }
 
-// uncomment these and put the correct values in the REQUIRE blocks
-
-/* TEST_CASE("Function: IQR 3", "[output_hidden]") {
-	std::vector<int> v = {1, 8, 15, 43, 82, 101, 110, 2456, 55345, 137556};
+TEST_CASE("Test 5: Duplicate Blocks", "[iqr]") {
+	vector<int> v = {1, 1, 1, 1, 10, 10, 10, 10};
 	Node* head = nullptr;
-	for(int i: v)
+	for (int i : v)
 		head = insertEnd(head, i);
 
-	REQUIRE(interQuartile(head) == ?);
-	
-    while (head != nullptr) 
-    {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-} */
+	REQUIRE(interQuartile(head) == 9.0f);
 
-/* TEST_CASE("Function: IQR 4", "[output_hidden]") {
-	std::vector<int> v = {2, 4, 4, 5, 6, 7, 8, 9, 10};
-	Node* head = nullptr;
-	for(int i: v)
-		head = insertEnd(head, i);
-
-	REQUIRE(interQuartile(head) == ?);
-	
-    while (head != nullptr) 
-    {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-} */
-
-/* TEST_CASE("Function: IQR 5", "[output_hidden]") {
-	std::vector<int> v = {1, 8, 15, 43, 82, 101, 110, 2456, 55345, 137556, 137576};
-	Node* head = nullptr;
-	for(int i: v)
-		head = insertEnd(head, i);
-
-	REQUIRE(interQuartile(head) == ?);
-	
-    while (head != nullptr) 
-    {
-        Node* temp = head;
-        head = head->next;
-        delete temp;
-    }
-} */
+	while (head != nullptr) {
+		Node* temp = head;
+		head = head->next;
+		delete temp;
+	}
+}
